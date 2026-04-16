dbca5685112cd09db5fb611a6b6a69a80f6d335f
sayoko <sayoko@WINDOWS-WORK>
2026-04-16 12:30:15 +0800

    精简代码和使用lfs

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+var btn = document.getElementById("btn");
+
+btn.onclick = function()
+{
+    window.location.href = "http://lmgzs.fun:444/";
+}
--- jump/js/index.js
+++
@@ -1,7 +0,0 @@
-
-var btn = document.getElementById("btn");
-
-btn.onclick = function()
-{
-    window.location.href = "http://lmgzs.fun:444/";
-}
